42c8a1eb875c1c95802a9643af68f7867118f09e
Marcos Vinicius <marcos2005.machado@gmail.com>
2026-03-18 14:36:50 -0300

    Troquei pra usar std::list (Ordered_List não está perfeito)

--- include/data_structures/list.hpp
+++ include/data_structures/list.hpp
@@ -2,53 +2,36 @@
 #include <list>
 
 template<typename T> class List{
-    private:
-        struct Node {
-            T val;
-            Node * next;
-            Node(const T & d): val(d), next(nullptr) {}
-        };
-
-        Node * _head = nullptr;
-        Node * _tail = nullptr;
-        std::mutex _mutex;
     public:
         List() {}
-        ~List() {
+        ~List() {}
+
+        void insert(T* obj) {
             std::lock_guard<std::mutex> lock(_mutex);
-            Node * current = _head;
-            while(current) {
-                Node * next = current->next;
-                delete current;
-                current = next;
-            }
+            _list.push_back(obj);
         }
 
-        void insert(T val) {
+        T * remove() {
             std::lock_guard<std::mutex> lock(_mutex);
-            Node * new_node = new Node(val);
-            if(!_head) {
-                _head = new_node;
-                _tail = new_node;
-            } else {
-                _tail->next = new_node;
-                _tail = new_node;
+            if(_list.empty()) {
+                throw std::runtime_error("List is empty");
             }
-        }
-
-        T remove() {
-            std::lock_guard<std::mutex> lock(_mutex);
-            if(!_head) throw std::runtime_error("List is empty");
-            Node * old_head = _head;
-            T val = old_head->val;
-            _head = old_head->next;
-            if(!_head) _tail = nullptr; // List is now empty
-            delete old_head;
-            return val;
+            T * obj = _list.front();
+            _list.pop_front();
+            return obj;
         }
 
         bool empty() {
             std::lock_guard<std::mutex> lock(_mutex);
-            return _head == nullptr;
+            return _list.empty();
         }
+
+        size_t size() {
+            std::lock_guard<std::mutex> lock(_mutex);
+            return _list.size();
+        }
+
+    private:
+        std::list<T *> _list;
+        std::mutex _mutex;
 };
--- include/data_structures/ordered_list.hpp
+++ include/data_structures/ordered_list.hpp
@@ -1,76 +1,32 @@
-template<typename O, typename C = void> class Ordered_List {
-    private:
-        struct Node {
-            O val;
-            Node * next;
-            Node(const O & d): val(d), next(nullptr) {}
-        };
+#include <list>
+#include <mutex>
 
-        Node * _head = nullptr;
-        std::mutex _mutex;
+template<typename O, typename C = void> class Ordered_List {
+    public:
+        using Observer = O;
+        using Condition = C;
+        class Iterator {
+            public:
+                Iterator(typename std::list<Observer>::iterator it): _it(it) {}
+                Observer & operator*() { return *_it; }
+                Observer * operator->() { return &(*_it); }
+                Iterator & operator++() { ++_it; return *this; }
+                bool operator!=(const Iterator & other) const { return _it != other._it; }
+            private:
+                typename std::list<Observer>::iterator _it;
+        };
     public:
         Ordered_List() {}
-        ~Ordered_List() {
+        ~Ordered_List() {}
+        void insert(Observer * o) {
             std::lock_guard<std::mutex> lock(_mutex);
-            Node * current = _head;
-            while(current) {
-                Node * next = current->next;
-                delete current;
-                current = next;
-            }
+            _list.push_back(*o);
+        }
+        void remove(Observer * o) {
+            std::lock_guard<std::mutex> lock(_mutex);
+            _list.remove(*o);
         }
 
-        void insert(O val) {
-            std::lock_guard<std::mutex> lock(_mutex);
-            Node * new_node = new Node(val);
-            if(!_head || new_node->val.rank() < _head->val.rank()) {
-                new_node->next = _head;
-                _head = new_node;
-            } else {
-                Node * current = _head;
-                while(current->next && current->next->val.rank() < new_node->val.rank()) {
-                    current = current->next;
-                }
-                new_node->next = current->next;
-                current->next = new_node;
-            }
-        }
-
-        void remove(O val) {
-            std::lock_guard<std::mutex> lock(_mutex);
-            if(!_head) return; // List is empty
-            if(_head->val == val) {
-                Node * old_head = _head;
-                _head = old_head->next;
-                delete old_head;
-                return;
-            }
-            Node * current = _head;
-            while(current->next && current->next->val != val) {
-                current = current->next;
-            }
-            if(current->next) { // Found the node to remove
-                Node * node_to_remove = current->next;
-                current->next = node_to_remove->next; // Bypass the node to remove
-                delete node_to_remove; // Free the memory of the removed node
-            }
-        }
-
-        class Iterator {
-            private:
-                Node * _current;
-
-            public:
-                Iterator(Node * start): _current(start) {}
-
-                O & operator*() { return _current->val; }
-                Iterator & operator++() { 
-                    if(_current) _current = _current->next; 
-                    return *this; 
-                }
-                bool operator!=(const Iterator & other) const { return _current != other._current; }
-        };
-        
-        Iterator begin() { return Iterator(_head); }
-        Iterator end() { return Iterator(nullptr); }
+        Iterator begin() { return Iterator(_list.begin()); }
+        Iterator end() { return Iterator(_list.end()); }
 };
