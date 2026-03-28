76c05e8db199e780acd41cbfe1c4a5240dcd4d10
Leoffran <leoffrano@gmail.com>
2026-03-28 17:23:37 -0300

    acho q a mensagem minima foi, tem q testar

--- include/message.hpp
+++ include/message.hpp
@@ -1,8 +1,40 @@
-
-#include <array>
+#pragma once
+#include <cstdint>
+#include <cstring>
+#include <cstddef>
 
 class Message {
     public:
+
+    // o tamanho maximo da mensagem
+    // 1500 -> ethernet, < protocol = 1024
+    static const unsigned int MAX_SIZE = 1024;
+
+    // construtor que cria uma mensagem vazia
+    Message() : _size(0) {
+        std::memset(_data, 0, MAX_SIZE);
+    }
+
+    // construtor que cria com conteúdo
+    Message(const void* data, unsigned int size) : _size(size) {
+        std::memcpy(_data, data, size);
+    }
     
+    // retorna o ponteiro pro array
+    // só leitura
+    const void* data() const { return _data; }
+    // ler e modificar
+    void*       data()       { return _data; }
+
+    // retorna quantos bytes válidos tem a mensagem
+    // o communicator passa pra protocol pra saber quantos bytes copiar pro ethernet
+    unsigned int size() const { return _size; }
+
+    // o communicator chama isso depois de receber dados pra registrar quantos bytes chegaram. (pra saber quando o conteúdo válido termina)
+    void set_size(unsigned int s) { _size = s; }
+
     private:
+
+    uint8_t      _data[MAX_SIZE];
+    unsigned int _size;
 };
