eafe552869aa138e009644606c9b8db67c4ecee7
Marcos Vinicius <marcos2005.machado@gmail.com>
2026-06-04 13:29:31 -0300

    Fiz pseudocódigo de execução do ResponsiveSmartData

--- include/smart_data/responsive_smart_data.hpp
+++ include/smart_data/responsive_smart_data.hpp
@@ -11,11 +11,21 @@ public:
     // typedef typename Unit::Get<UNIT>::Type Value; (pode ser útil para acessar o tipo de dado numérico correspondente à unidade, se necessário)
 
 public:
-    ResponsiveSmartData(Communicator* comm) : _comm(comm) {
+    ResponsiveSmartData(Communicator* comm) : _comm(comm), _port(Transducer::PORT) {
         // O Transducer deve definir uma porta específica para este SmartData, que será usada para receber os dados
-        // Exemplo: _port = Transducer::PORT;
         // Depois, o SmartData se registra como observador nessa porta para receber os dados do Transducer
-        // attach_comm(comm, _port);
+        attach_comm(comm, _port);
+    }
+
+    void run() {
+        // Este método pode ser chamado para iniciar o processamento do SmartData, se necessário
+        // Por exemplo, pode ser usado para configurar timers, iniciar threads, etc.
+        // while true
+        // Message msg;
+        // if receive(&msg && msg é Interest) {
+        //     uint64_t period = faz o cast do data
+        //     if period not in conjunto de periodos que já envio {
+        //         cria thread periódica que a cada period nanosegundos envia Response
     }
 
     void attach_comm(Communicator* comm, Protocol::Port port) {
